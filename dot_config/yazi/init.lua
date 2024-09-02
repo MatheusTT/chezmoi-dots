bab98c77a91dae54bbcf8566b4d76e5082f17793
MatheusTT <mttomaz@duck.com>
2024-09-02 09:01:00 -0300

    Added some plugins and keybindings to yazi

---
+++ dot_config/yazi/init.lua
@@ -0,0 +1,24 @@
+-- Show user/group of files in status bar
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if h == nil or ya.target_family() ~= "unix" then
+		return ui.Line {}
+	end
+
+	return ui.Line {
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		ui.Span(":"),
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		ui.Span(" "),
+	}
+end, 500, Status.RIGHT)
+
+-- Plugins
+--- Full Border
+require("full-border"):setup {
+	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+	type = ui.Border.PLAIN,
+}
+
+--- Git
+require("git"):setup()

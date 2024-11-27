ed8421aae66616c01f2392871db1c681e2b80e0b
MatheusTT <mttomaz@duck.com>
2024-11-27 00:42:13 -0300

    Improved weather report

--- dot_config/eww/widgets/bar/scripts/executable_weather.sh
+++ dot_config/eww/widgets/bar/scripts/executable_weather.sh
@@ -1,3 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-curl -s "wttr.in/Curitiba?format=1" | awk '{print $2 " " $1}'
+declare -a weather
+weather_report="$(curl -s "wttr.in/Curitiba?format=%c+%C+%h+%t+%f+%m+%p")"
+# shellcheck disable=SC2048
+for i in ${weather_report[*]}; do
+  weather+=("$i")
+done
+
+echo "{
+  \"cond\":          \"${weather[0]}\",
+  \"text_cond\":     \"${weather[1]}\",
+  \"humidity\":      \"${weather[2]}\",
+  \"temp\":          \"${weather[3]}\",
+  \"feels_like\":    \"${weather[4]}\",
+  \"moon\":          \"${weather[5]}\",
+  \"precipitation\": \"${weather[6]}\"
+}"
